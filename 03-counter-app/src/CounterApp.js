ece096278e6ce240b04771b8190b8c59fa05171f
rox <rox050996@gmail.com>
2022-05-03 20:00:55 -0500

    substract and reset buttons

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 // import React, { Fragment } from 'react';
 
 
-const PrimeraApp = ({ value }) =>{
+const PrimeraApp = ({ value = 10 }) =>{
+
+    const [counter,setCounter] = useState(value)
 
     //handleAdd
-    const handleAdd = (e) =>{
-         console.log(e)
+    const handleAdd = () =>{
+        setCounter ( counter + 1);
 
+        //setCounter ( (c) => c+1)
+        // return () => console.log('Hola Mundo');
+    }
+
+    const handleSubstract = () =>{
+        setCounter ( counter - 1);
+
+        //setCounter ( (c) => c+1)
+        // return () => console.log('Hola Mundo');
+    }
+
+    const handleReset = () =>{
+        setCounter ( value );
+
+        //setCounter ( (c) => c+1)
         // return () => console.log('Hola Mundo');
     }
 
     return (
         <>
             <h1> CounterApp </h1>
-            <p>{ value }</p>
+            <p>{ counter }</p>
 
             <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
         </>
     );
 }
