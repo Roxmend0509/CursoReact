9f9fd683d1aa3a1c74198a4142b936a918757a9c
rox <rox050996@gmail.com>
2022-05-04 10:33:51 -0500

    update a React 18+

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,25 @@
+
+//React -17 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import PrimeraApp from './PrimeraApp';
+
+// import CounterApp from './CounterApp';
+// import './index.css'
+
+// const divRoot = document.querySelector('#root');
+
+// ReactDOM.render(<CounterApp value={10} />,divRoot);
+
+//React 18+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 // import PrimeraApp from './PrimeraApp';
 
 import CounterApp from './CounterApp';
 import './index.css'
 
 const divRoot = document.querySelector('#root');
+const root = createRoot(divRoot);
 
-ReactDOM.render(<CounterApp value={10} />,divRoot);
+root.render(<CounterApp value={10} />);
