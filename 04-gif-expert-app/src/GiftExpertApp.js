cce901292f92e3fee6da9c4eafd5d5096d1dd52d
rox <roxana.mendoza@htech.com>
2022-05-06 17:05:43 -0500

    Component AddCategory and creacion de listado categorias

---
+++ 04-gif-expert-app/src/GiftExpertApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+
+export const GiftExpertApp = () => {
+
+    // const categories = ['Demon Slayer','Jeff Hardy','Puca'];
+
+    const [categories,setCategories] = useState(['Demon Slayer','Jeff Hardy','Puca']);
+
+    // const handleAdd = () =>{
+    //     setCategories( cat => [...cat,'Olaf']);
+    // }
+
+  return (
+    <>
+        <h2>GiftExpertApp</h2>
+        <AddCategory />
+        <hr />
+        <ol>
+            {
+                categories.map( (category) => {
+                    return <li key = {category}>{category}</li>
+                } )
+            }
+        </ol>
+    </>
+  )
+}
