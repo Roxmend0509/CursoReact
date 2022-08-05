365cb295558bdd49c15689b2cf8b5cded17318f4
Rox Mendoza <rox050996@gmail.com>
2022-08-05 10:25:08 -0500

    App Gift

---
+++ 04-gif-expert-app-vite/src/GiftExpertApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const GiftExpertApp = () => {
+
+    const [categories, setCategories] = useState()
+
+  return (
+    <>
+        { /* Titulo */}
+        <h1>GiftExpertApp</h1>
+
+        { /* Input */}
+
+        { /* Listado de GIFS */}
+            { /* Gif Item */}
+    </>
+  )
+}
