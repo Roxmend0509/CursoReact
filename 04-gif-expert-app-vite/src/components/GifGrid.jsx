f846f88213706fcf7166f13656884cbe53767a81
Rox Mendoza <rox050996@gmail.com>
2022-08-08 13:07:56 -0500

    Finish app Gifs

--- 04-gif-expert-app-vite/src/components/GifGrid.jsx
+++ 04-gif-expert-app-vite/src/components/GifGrid.jsx
@@ -1,25 +1,19 @@
 import { useEffect, useState } from "react";
 
 import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([])
-
-  const getImages = async() =>{
-    const newImages = await getGifs(category);
-    setImages(newImages)
-  }
-
-    useEffect(() =>{
-      getImages();
-    }, [ ])
-    
+const { images, isLoading } = useFetchGifs( category )
 
   return (
     <>
         <h3> {category} </h3>
+        {
+          isLoading && (<h2> Cargando... </h2>)
+        }
         <div className="card-grid">
           {
             images.map (( image ) => (
@@ -29,6 +23,7 @@ export const GifGrid = ({ category }) => {
             ))
           }
           </div>
+          <span className="credits"> Rox Mendoza </span>
     </>
   )
 }
